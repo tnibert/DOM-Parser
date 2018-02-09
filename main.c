6c9bb66174ca490d5e8690b46ecdeda4fe2fef4f
Tim Nibert <docz2a@gmail.com>
2018-02-09 19:42:17 +1100

    initial planning

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+struct node {
+    node * parent;
+    node * child;
+    node * sibling;
+}
+
+node * add_node(char * data, parentnode)
+{
+    // stub
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+
+    char ch;
+
+    for(node * cur = NULL; (ch = getc(fp)) != EOF);)
+    {
+        // if char is '<'
+            // create new child node and enter
+    }
+
+    return 0;
+}
