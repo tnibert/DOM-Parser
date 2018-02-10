7321a810d688cb67febf3f37696ef5739a0f72e4
Tim Nibert <docz2a@gmail.com>
2018-02-10 12:41:41 +1100

    add_node() returns something now, error checking for cli arg

--- main.c
+++ main.c
@@ -1,19 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+/*
+compile with gcc main.c
+run with ./a.out htmlfile.html
+*/
 
 struct node {
     struct node * parent;
     struct node * child;
-    struct node * sibling;
+    struct node * nextsibling;  // we only need to hold a reference to next sibling,
+                                // and we only need to reference first child
+    char * tag;
+    char * data;
+    // we will later include options, e.g. href=, id=, etc
 };
 
 struct node * add_node(char * data, struct node * parentnode)
 {
-    // stub
-    return NULL;
+    struct node * newnode = malloc(sizeof(struct node));
+    return newnode;
 }
 
 int main(int argc, char *argv[])
 {
+
+    if(argc < 2)
+    {
+        printf("Must specify html/xml file as command line argument.\n");
+        return -1;
+    }
 
     char ch;
     FILE * fp = fopen(argv[1], "r");
@@ -25,6 +41,9 @@ int main(int argc, char *argv[])
             // create new child node and enter
     }
     printf("\n");
+
+    // test
+    struct node * testnode = add_node("test data", NULL);
 
     return 0;
 }
