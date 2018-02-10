10c178a8a98386b61f1cbaed075e6e59696e0f3e
Tim Nibert <docz2a@gmail.com>
2018-02-10 13:10:00 +1100

    implemented add_node() function, still need to test more

--- main.c
+++ main.c
@@ -8,18 +8,61 @@ run with ./a.out htmlfile.html
 
 struct node {
     struct node * parent;
-    struct node * child;
-    struct node * nextsibling;  // we only need to hold a reference to next sibling,
-                                // and we only need to reference first child
+    struct node * firstchild;
+    struct node * nextsibling;  // hmm...
+
     char * tag;
     char * data;
     // we will later include options, e.g. href=, id=, etc
 };
 
-struct node * add_node(char * data, struct node * parentnode)
+struct node * add_node(char * tag, char * data, struct node * parentnode)
 {
     struct node * newnode = malloc(sizeof(struct node));
+    newnode->parent = parentnode;
+
+    if(parentnode != NULL)
+    {
+        if(parentnode->firstchild == NULL)
+        {
+            parentnode->firstchild = newnode;
+        }
+        else
+        {
+            // iterate through until we have the last sibling
+            struct node * lastsibling;
+            for(lastsibling = parentnode->firstchild; lastsibling->nextsibling != NULL; lastsibling = lastsibling->nextsibling);
+            lastsibling->nextsibling = newnode;
+            // you know, we probably could have done it backward and just traversed the tree backward and everything would have been fine...
+            // eg, store lastchild and prevsibling in node struct
+        }
+    }
+    else        // if we are the root node
+    {
+        // newnode->parent is already NULL
+        newnode->nextsibling = NULL;
+    }
+
+    // set child to NULL
+    // maybe we can build this from the deepest level though, just for fun, something to think about
+    newnode->firstchild = NULL;
+
+    // set xml data
+    newnode->tag = tag;
+    newnode->data = data;
+
     return newnode;
+}
+
+// use this to test the add_node() function
+char * node_to_str(struct node * curnode)
+{
+    return "This function is still a stub";
+}
+
+void traverse(struct node * root)
+{
+    return;
 }
 
 int main(int argc, char *argv[])
@@ -32,7 +75,7 @@ int main(int argc, char *argv[])
     }
 
     char ch;
-    FILE * fp = fopen(argv[1], "r");
+    FILE * fp = fopen(argv[1], "r");        // should probably error check this too
 
     for(struct node * cur = NULL; (ch = getc(fp)) != EOF;)
     {
@@ -43,7 +86,8 @@ int main(int argc, char *argv[])
     printf("\n");
 
     // test
-    struct node * testnode = add_node("test data", NULL);
+    struct node * testheadnode = add_node("html", "test data", NULL);
+    struct node * testchildnode = add_node("head", "more test data", testheadnode);
 
     return 0;
 }
